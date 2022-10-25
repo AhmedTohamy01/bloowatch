a3bd08f5d3b88eb0ef41c2fc7525f34d97e2fa79
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2022-10-25 22:42:44 +0200

    feat [Navbar] : add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+import { ShoppingCart } from 'lucide-react'
+
+/*---> Component <---*/
+export const Navbar = () => {
+  return (
+    <NavbarWrapper>
+      <LogoWrapper>
+        <Logo alt='BlooWatch logo' src='./images/logo.png' />
+      </LogoWrapper>
+      <LinksWrapper>
+        <Link>SHOP</Link>
+        <Link>BLOG</Link>
+        <Link>SEARCH</Link>
+        <Link>
+          <CartIcon />
+          CART
+        </Link>
+      </LinksWrapper>
+    </NavbarWrapper>
+  )
+}
+
+/*---> Styles <---*/
+const NavbarWrapper = styled.div`
+  /* border: 1px solid red; */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 40px 20px 80px;
+
+  @media (max-width: 720px) {
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 20px;
+  }
+`
+
+const LogoWrapper = styled.div`
+  /* border: 1px solid green; */
+
+  @media (max-width: 720px) {
+    margin-bottom: 20px;
+  }
+`
+
+const Logo = styled.img`
+  /* border: 1px solid yellow; */
+`
+
+const LinksWrapper = styled.div`
+  /* border: 1px solid green; */
+  display: flex;
+  align-items: center;
+`
+
+const Link = styled.div`
+  /* border: 1px solid yellow; */
+  margin-left: 100px;
+  font-family: 'OpenSansSemibold';
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  @media (max-width: 900px) {
+    margin-left: 50px;
+  }
+
+  @media (max-width: 720px) {
+    margin-left: 0px;
+    margin-right: 30px;
+  }
+`
+
+const CartIcon = styled(ShoppingCart)`
+  /* border: 1px solid yellow; */
+  color: gray;
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+`
