c24ac1ac368d450c4b5408ba9f4421107e0dcb6f
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2022-10-26 21:00:02 +0200

    feat [HomePage] : import HomeMainContent in HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from '../../components/Navbar/Navbar'
 import { PageTitle } from '../../components/PageTitle/PageTitle'
+import { HomeMainContent } from '../../components/HomeMainContent/HomeMainContent'
 
 export const HomePage = () => {
   return (
     <>
       <Navbar />
-      <PageTitle title='SURFING'/>
+      <PageTitle title='SURFING' />
+      <HomeMainContent />
     </>
   )
 }
