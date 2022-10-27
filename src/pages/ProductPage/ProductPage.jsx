c2a2d2fd86b18b4e36769e6e470d9ac69cfffb06
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2022-10-28 00:12:46 +0200

    feat [ProductPage] : import Footer component

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from '../../components/Navbar/Navbar'
 import { ProductMainContent } from '../../components/ProductMainContent/ProductMainContent'
 import { PageTitle } from '../../components/PageTitle/PageTitle'
+import { Footer } from '../../components/Footer/Footer'
 
 export const ProductPage = () => {
   return (
@@ -8,6 +9,7 @@ export const ProductPage = () => {
       <Navbar />
       <PageTitle title='TUNDER SHORT BOARD' />
       <ProductMainContent />
+      <Footer />
     </>
   )
 }
