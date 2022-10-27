faf83b7e2369d67ce712c0efbb99c7892ff2b6d0
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2022-10-28 00:54:24 +0200

    feat [ProductPage] : import ProductDetails & RecommendedProducts in ProductPage

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -2,6 +2,8 @@ import { Navbar } from '../../components/Navbar/Navbar'
 import { ProductMainContent } from '../../components/ProductMainContent/ProductMainContent'
 import { PageTitle } from '../../components/PageTitle/PageTitle'
 import { Footer } from '../../components/Footer/Footer'
+import { ProductDetails } from '../../components/ProductDetails/ProductDetails'
+import { RecommendedProducts } from '../../components/RecommendedProduct/RecommendedProducts'
 
 export const ProductPage = () => {
   return (
@@ -9,6 +11,8 @@ export const ProductPage = () => {
       <Navbar />
       <PageTitle title='TUNDER SHORT BOARD' />
       <ProductMainContent />
+      <ProductDetails />
+      <RecommendedProducts />
       <Footer />
     </>
   )
