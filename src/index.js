10aa6d759168048c6af742532c78c8d36f99328d
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2022-10-27 22:48:53 +0200

    feat [index.js] : wrap App with MainContextProvider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from "react-router-dom"
+import { MainContextProvider } from './context/MainContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <MainContextProvider>
+        <App />
+      </MainContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
